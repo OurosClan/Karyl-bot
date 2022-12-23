c02a74c9fa3386f0c3c64e434d2ff4f313c0b05b
Ice <easibaja06@gmail.com>
2022-12-23 17:52:04 -0600

    test args 2

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length); /*.split(/ +/)*/
+  const args = message.content.slice(prefix.length.split()); // / +/
   const command = args.shift().toLowerCase();
 
   // Message array not used yet
